10d05d0fa276b0e869db1e04280bb96f8aea524f
Renzo Lucioni <renzo@renzolucioni.com>
2016-06-17 12:56:27 -0400

    Use Flask-Compress to gzip responses

--- typesetter/static/jsx/main.jsx
+++ typesetter/static/jsx/main.jsx
@@ -1,30 +1,37 @@
 var DynamicSearch = React.createClass({
 
     getInitialState: function() {
+        console.log('Getting initial state.');
         return {searchString: ''};
     },
 
     componentDidMount: function() {
+        console.log('Component did mount, getting data.');
         this.serverRequest = $.get(this.props.source, function(words) {
+            console.log(words);
             this.setState({words: words});
         }.bind(this));
     },
 
     componentWillUnmount: function() {
+        console.log('Component will unmount, aborting request.');
         this.serverRequest.abort();
     },
 
     handleChange: function(event) {
+        console.log('Handling change, setting state.');
         this.setState({searchString: event.target.value});
     },
 
     render: function() {
+        console.log('Rendering.');
 
         var words = this.state.words || [];
         var searchString = this.state.searchString.trim().toLowerCase();
 
         if (searchString.length > 0) {
             words = words.filter(function(word) {
+                console.log(`Checking if ${word} matches ${searchString}.`)
                 return word.toLowerCase().match(searchString);
             });
         }
@@ -33,7 +40,7 @@ var DynamicSearch = React.createClass({
             <div>
                 <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Search..."/>
                 <ul>
-                    {words.map(function(word) { return <li>{word}</li> })}
+                    {words.map(function(word) { return <li key={word}>{word}</li> })}
                 </ul>
             </div>
         )
--- typesetter/typesetter.py
+++ typesetter/typesetter.py
@@ -1,6 +1,7 @@
 import argparse
 
 from flask import Flask, render_template, jsonify
+from flask.ext.compress import Compress
 from livereload import Server
 
 
@@ -24,6 +25,8 @@ app.config.update(
     JSONIFY_PRETTYPRINT_REGULAR=False,
 )
 
+Compress(app)
+
 
 @app.route('/')
 def index():
@@ -32,16 +35,16 @@ def index():
 
 @app.route('/api/words')
 def words():
-    # with open('typesetter/data/words.txt') as words:
-    #     return jsonify([word.strip('\n') for word in words])
-    return jsonify([
-        'apple',
-        'orange',
-        'raspberry',
-        'peach',
-        'strawberry',
-        'banana',
-    ])
+    with open('typesetter/data/words.txt') as words:
+        return jsonify([word.strip('\n') for word in words])
+    # return jsonify([
+    #     'apple',
+    #     'orange',
+    #     'raspberry',
+    #     'peach',
+    #     'strawberry',
+    #     'banana',
+    # ])
 
 
 server = Server(app.wsgi_app)
